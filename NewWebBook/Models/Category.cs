249c9f3734f2ffaddcd3461bfcc64658d62c571a
jogindrakumar <joginder9868@outlook.com>
2023-01-30 07:10:01 +0530

    set db

--- NewWebBook/Models/Category.cs
+++ NewWebBook/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace NewWebBook.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewWebBook.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
