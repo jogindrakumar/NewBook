e92a24109503dbbe935e20abff9540027014063d
jogindrakumar <joginder9868@outlook.com>
2023-02-12 19:15:06 +0530

    create-cateogry-link-and-show-table

--- NewWebBook/Controllers/CategoryController.cs
+++ NewWebBook/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewWebBook.Data;
+using NewWebBook.Models;
 
 namespace NewWebBook.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }
